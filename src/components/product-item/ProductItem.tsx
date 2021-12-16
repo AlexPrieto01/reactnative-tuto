1e9f8f4c2c514b61e2e23c82c3a2bfe9cc9a7ae8
AlexPrieto01 <92782383+AlexPrieto01@users.noreply.github.com>
2021-12-16 12:50:13 +0100

    Intentando hacer que el carrito funcione.

--- src/components/product-item/ProductItem.tsx
+++ src/components/product-item/ProductItem.tsx
@@ -1,46 +1,50 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { Button, Text } from 'react-native-elements';
 import { Image } from 'react-native-elements/dist/image/Image';
+
+import { useNavigation } from '@react-navigation/core';
 import Counter from '../counter/Counter';
 import styles from './ProductItem-style';
 
-const ProductItem = ({ product, onAddItem }) => {
-    const [itemList, setItem] = useState(1);
 
-    const onPressCarrito = () => {
-        onAddItem(product, itemList)
-        console.log(itemList);
+const ProductItem = ({ product, onAddItemToCart }) => {
+    const navigation = useNavigation<any>();
+    const [numItems, setNumItems] = useState(1);
+
+    const onPressCard = () => {
+        navigation.navigate('product-details', { item: product });
     };
+
+    const onPressAddButton = () => {
+        onAddItemToCart(product, numItems);
+    };
+
     return (
         <View style={styles.card}>
-            <View style={styles.topContainer}>
-                <Image
-                    style={styles.cardImage}
-                    source={{
-                        uri: product.image
-                    }}
-                />
-                <View style={styles.brandContainer}>
-                    <Text style={styles.brandText}>{product.brand}</Text>
+            <TouchableOpacity onPress={onPressCard}>
+                <View style={styles.topContainer}>
+                    <Image
+                        style={styles.cardImage}
+                        source={{
+                            uri: product.image
+                        }}
+                    />
+                    <View style={styles.brandContainer}>
+                        <Text style={styles.brandText}>{product.brand}</Text>
+                    </View>
                 </View>
-            </View>
 
-            <View style={styles.middleContainer}>
-                <Text style={styles.nameText}>{product.name}</Text>
-                <Text style={styles.priceText}>{`${product.unitPrice}€`}</Text>
-            </View>
-
+                <View style={styles.middleContainer}>
+                    <Text style={styles.nameText}>{product.name}</Text>
+                    <Text style={styles.priceText}>{`${product.unitPrice}€`}</Text>
+                </View>
+            </TouchableOpacity>
             <View style={styles.counterContainer}>
-                <Counter />
+                <Counter value={numItems} onChangeValue={setNumItems} />
             </View>
-
-            <View style={styles.actionContainer}>
-                <Button
-                    title="Añadir al carrito"
-                    onPress={onPressCarrito}
-                />
-
+            <View>
+                <Button onPress={onPressAddButton} title="Añadir al carrito" />
             </View>
         </View>
     );
--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,51 +1,38 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect } from 'react';
-import { FlatList, SafeAreaView, View } from 'react-native';
-import { Button, Text } from 'react-native-elements';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
+import { LinearProgress } from 'react-native-elements';
 import styles from './product-styles';
-import Counter from '../../components/counter/Counter';
-import ProductsService from '../../services/ProductServices';
-import Load from '../../components/loader/Loader';
+
 import ProductItem from '../../components/product-item/ProductItem';
+import Load from '../../components/loader/Loader';
+import ProductsService from '../../services/ProductServices';
 
-
-const Products = () => {
+const Products = ({ onAddItemToCart }) => {
   const [products, setProducts] = useState([]);
-  const navigation = useNavigation<any>();
-
-  const onPressButton = () => {
-    navigation.navigate('product-details');
-  };
 
   useEffect(() => {
-    console.log('useeffect')
-
-    const asyncCall = async () => {
+    const getAsyncProducts = async () => {
       const result = await ProductsService.getProducts();
       setProducts(result.data);
     };
 
-    asyncCall();
-
+    getAsyncProducts();
   }, []);
 
-  if (products.length === 0) { return <Load /> }
+  if (products.length === 0) {
+    return <Load />;
+  }
 
   return (
-
     <SafeAreaView style={styles.container}>
-      <Text h4>Bienvenido a la tienducha!</Text>
       <FlatList
         data={products}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => {
-          return <ProductItem product={item} />;
+          return <ProductItem product={item} onAddItemToCart={onAddItemToCart} />;
         }}
       />
-      <Button onPress={onPressButton} title="Ir a la página de detalles" />
     </SafeAreaView>
-
   );
 };
 
